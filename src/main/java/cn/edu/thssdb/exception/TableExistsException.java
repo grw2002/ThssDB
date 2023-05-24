170e927985630660e3eec228acdaa3007a393298
王陆离 <2970012705@qq.com>
2023-05-24 16:13:57 +0800

    Exceptions

---
+++ src/main/java/cn/edu/thssdb/exception/TableExistsException.java
@@ -0,0 +1,8 @@
+package cn.edu.thssdb.exception;
+
+public class TableExistsException extends RuntimeException {
+    @Override
+    public String getMessage() {
+        return "Exception: table already exists!";
+    }
+}
